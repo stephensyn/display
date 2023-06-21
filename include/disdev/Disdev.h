a02e856516c243a8fc72fd25f64ce96b3f933248
stephen <stephensyh@gmail>
2023-06-21 16:55:00 +0800

    修改了路径名称，准备写一个显示设备类

---
+++ include/disdev/Disdev.h
@@ -0,0 +1,17 @@
+#ifndef _DISDEV_H_
+#define _DISDEV_H_
+
+extern "C"
+{
+#include <oled/GUI_Paint.h>
+#include <oled/GUI_BMPfile.h>
+#include <oled/Debug.h>
+
+#include <stdlib.h> // malloc() free()
+#include <math.h>
+
+#include <oled/DEV_Config.h>
+#include <oled/OLED_1in5.h>
+}
+
+#endif
