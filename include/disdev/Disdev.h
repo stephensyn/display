2b98abc537fe197f3e88d0c513e806305e0d0839
stephen <stephensyh@gmail>
2023-06-22 22:12:44 +0800

    完善类中

--- include/disdev/Disdev.h
+++ include/disdev/Disdev.h
@@ -6,13 +6,131 @@ extern "C"
 #include <oled/GUI_Paint.h>
 #include <oled/GUI_BMPfile.h>
 #include <oled/Debug.h>
-
 #include <stdlib.h> // malloc() free()
 #include <math.h>
-
 #include <oled/DEV_Config.h>
 #include <oled/OLED_1in5.h>
+#include <oled/fonts.h>
 }
+
+/**
+ * Image attributes
+ **/
+typedef struct
+{
+    UBYTE *Image;
+    UWORD Width;
+    UWORD Height;
+    UWORD WidthMemory;
+    UWORD HeightMemory;
+    UWORD Color;
+    UWORD Rotate;
+    UWORD Mirror;
+    UWORD WidthByte;
+    UWORD HeightByte;
+    UWORD Scale;
+} PAINT;
+extern PAINT Paint;
+
+/**
+ * Display rotate
+ **/
+#define ROTATE_0 0
+#define ROTATE_90 90
+#define ROTATE_180 180
+#define ROTATE_270 270
+
+/**
+ * Display Flip
+ **/
+typedef enum
+{
+    MIRROR_NONE = 0x00,
+    MIRROR_HORIZONTAL = 0x01,
+    MIRROR_VERTICAL = 0x02,
+    MIRROR_ORIGIN = 0x03,
+} MIRROR_IMAGE;
+#define MIRROR_IMAGE_DFT MIRROR_NONE
+
+/**
+ * image color
+ **/
+#define WHITE 0xFFFF
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define BRED 0XF81F
+#define GRED 0XFFE0
+#define GBLUE 0X07FF
+#define RED 0xF800
+#define MAGENTA 0xF81F
+#define GREEN 0x07E0
+#define CYAN 0x7FFF
+#define YELLOW 0xFFE0
+#define BROWN 0XBC40
+#define BRRED 0XFC07
+#define GRAY 0X8430
+
+#define IMAGE_BACKGROUND WHITE
+#define FONT_FOREGROUND BLACK
+#define FONT_BACKGROUND WHITE
+
+/**
+ * The size of the point
+ **/
+typedef enum
+{
+    DOT_PIXEL_1X1 = 1, // 1 x 1
+    DOT_PIXEL_2X2,     // 2 X 2
+    DOT_PIXEL_3X3,     // 3 X 3
+    DOT_PIXEL_4X4,     // 4 X 4
+    DOT_PIXEL_5X5,     // 5 X 5
+    DOT_PIXEL_6X6,     // 6 X 6
+    DOT_PIXEL_7X7,     // 7 X 7
+    DOT_PIXEL_8X8,     // 8 X 8
+} DOT_PIXEL;
+#define DOT_PIXEL_DFT DOT_PIXEL_1X1 // Default dot pilex
+
+/**
+ * Point size fill style
+ **/
+typedef enum
+{
+    DOT_FILL_AROUND = 1, // dot pixel 1 x 1
+    DOT_FILL_RIGHTUP,    // dot pixel 2 X 2
+} DOT_STYLE;
+#define DOT_STYLE_DFT DOT_FILL_AROUND // Default dot pilex
+
+/**
+ * Line style, solid or dashed
+ **/
+typedef enum
+{
+    LINE_STYLE_SOLID = 0,
+    LINE_STYLE_DOTTED,
+} LINE_STYLE;
+
+/**
+ * Whether the graphic is filled
+ **/
+typedef enum
+{
+    DRAW_FILL_EMPTY = 0,
+    DRAW_FILL_FULL,
+} DRAW_FILL;
+
+/**
+ * Custom structure of a time attribute
+ **/
+typedef struct
+{
+    UWORD Year;  // 0000
+    UBYTE Month; // 1 - 12
+    UBYTE Day;   // 1 - 30
+    UBYTE Hour;  // 0 - 23
+    UBYTE Min;   // 0 - 59
+    UBYTE Sec;   // 0 - 59
+} PAINT_TIME;
+extern PAINT_TIME sPaint_time;
 
 #include <iostream>
 
@@ -73,6 +191,14 @@ public:
     // 显示时间:在图像缓存中，在 (Xstart Ystart) 为左顶点，显示一段时间，可以选择Ascii码可视字符字库、字体前景色、字体背景色；
     void Paint_DrawTime(UWORD Xstart, UWORD Ystart, PAINT_TIME *pTime, sFONT *Font,
                         UWORD Color_Background, UWORD Color_Foreground);
+    void Paint_SetRotate(UWORD Rotate);
+    void Paint_SetMirroring(UBYTE mirror);
+    void Paint_DrawRectangle(UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend,
+                             UWORD Color, DOT_PIXEL Line_width, DRAW_FILL Draw_Fill);
+    void Paint_DrawCircle(UWORD X_Center, UWORD Y_Center, UWORD Radius,
+                          UWORD Color, DOT_PIXEL Line_width, DRAW_FILL Draw_Fill);
+    void Paint_DrawBitMap(const unsigned char *image_buffer);
+    void Paint_DrawBitMap_Block(const unsigned char *image_buffer, UBYTE Region);
 };
 
 #endif
